d98c0a707fa144eb9e5a419e9856b5bfdbe2163d
codemma <emma.tysk@gmail.com>
2020-10-11 14:05:09 +0300

    added functionality which dynamically adds and removes css class on mouse enter and mouse leave

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,36 +3,53 @@
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <section class="first-section">
       <div class="l-heading">Hej hej!</div>
-      <div class="l-heading">My name is Emma Tysk</div>
+      <div class="l-heading">Emma Tysk</div>
     </section>
     <section class="container">
       <div class="gallery-container">
-        <div class="gallery-item">
+        <div
+          @mouseenter="mouseOver"
+          @mouseleave="mouseLeave"
+          class="gallery-item"
+        >
           <div class="image">
+            <!-- @mouseover="mouseOver" -->
             <img
-              src="https://source.unsplash.com/1600x900/?nature"
+              src="https://source.unsplash.com/1600x900/?art"
               alt="brezza design"
             />
+            <div @mouseover.stop="childMouseOver" class="hover">
+              <div class="text">Hover test</div>
+            </div>
           </div>
-          <div class="text">Brezza</div>
         </div>
       </div>
       <div class="gallery-container">
-        <div class="gallery-item">
+        <div
+          @mouseenter="mouseOver"
+          @mouseleave="mouseLeave"
+          class="gallery-item"
+        >
           <div class="image">
             <img
               src="https://source.unsplash.com/1600x900/?art"
               alt="brezza design"
             />
+            <div class="hover">
+              <div class="text">Hover test</div>
+            </div>
           </div>
-          <div class="text">Brezza</div>
         </div>
       </div>
       <div class="gallery-container">
-        <div class="gallery-item">
+        <div
+          class="gallery-item"
+          @mouseenter="mouseOver"
+          @mouseleave="mouseLeave"
+        >
           <div class="image">
             <img
-              src="https://source.unsplash.com/1600x900/?art"
+              src="https://source.unsplash.com/1600x900/?people"
               alt="brezza design"
             />
           </div>
@@ -40,7 +57,11 @@
         </div>
       </div>
       <div class="gallery-container">
-        <div class="gallery-item">
+        <div
+          class="gallery-item"
+          @mouseenter="mouseOver"
+          @mouseleave="mouseLeave"
+        >
           <div class="image">
             <img
               src="https://source.unsplash.com/1600x900/?nature"
@@ -60,8 +81,25 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {};
+  },
   components: {
     HelloWorld,
+  },
+  methods: {
+    mouseLeave(event) {
+      console.log("mouseleave", event.target.classList);
+      event.target.className += " transition-out";
+    },
+    mouseOver(event) {
+      console.log("mouseover", event.target.classList);
+      event.target.classList.remove("transition-out");
+      this.hover = true;
+    },
+    childMouseOver(event) {
+      //event.stopPropagation();
+    },
   },
 };
 </script>
@@ -80,24 +118,62 @@ export default {
   height: 100%;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-auto-rows: 100px 300px;
-  grid-gap: 10px;
+  grid-auto-rows: 200px 300px;
+  grid-gap: 20px;
 }
 .gallery-item {
   width: 100%;
   height: 100%;
   position: relative;
+  overflow: hidden;
 }
 
 .gallery-item .image {
   width: 100%;
   height: 100%;
-  overflow: hidden;
+  // overflow: hidden;
 }
 .gallery-item .image img {
   width: 100%;
   height: 100%;
   object-fit: cover;
+  object-position: 50% 50%;
+  cursor: pointer;
+}
+.transition-out .image img {
+  -webkit-transform: translateZ(0);
+  animation: leaves 0.8s ease-in;
+}
+
+@keyframes leaves {
+  0% {
+    opacity: 10%;
+    transform: scale(1.05);
+    -webkit-transform: scale(1.05);
+  }
+  100% {
+    opacity: 100%;
+    transform: scale(1);
+    -webkit-transform: scale(1);
+  }
+}
+
+.gallery-item:hover .hover {
+  visibility: visible;
+  //transform: translate(10px, 10px);
+}
+.gallery-item .hover {
+  visibility: hidden;
+  background-color: pink;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  transition: 0.1s ease-in-out;
+  top: 0;
+  .text {
+    pointer-events: none;
+    color: white;
+  }
 }
 .first-section {
   text-align: left;
