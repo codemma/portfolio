1699be17a0552e7b4062036e6af890f841dbb956
codemma <emma.tysk@gmail.com>
2020-10-21 21:50:47 +0300

    switched to svg

--- src/views/Kgt.vue
+++ src/views/Kgt.vue
@@ -26,9 +26,9 @@
     <div class="h3 m-b-30">App design</div>
     <div class="gallery-container">
       <div class="blue-divider">
-        <img src="../assets/kgt/divider.png" alt="kgt design" />
+        <img src="../assets/kgt/divider.svg" alt="kgt design" />
       </div>
-      <div class="gallery-3-col m-b-50">
+      <div class="gallery-flex m-b-50">
         <div class="image">
           <img src="../assets/kgt/screen-1.png" alt="kgt design" />
         </div>
@@ -43,20 +43,20 @@
     <div class="h3 m-b-30">Illustrations</div>
     <div class="gallery-2-col">
       <div class="image">
-        <img src="../assets/kgt/getApp.png" alt="kgt  design" />
+        <img src="../assets/kgt/getApp.svg" alt="kgt  design" />
       </div>
       <div class="h4 text-left">1. Get the app</div>
       <div class="h4 text-right">2. Search and apply for work</div>
       <div class="image">
-        <img src="../assets/kgt/chooseWork.png" alt="kgt  design" />
+        <img src="../assets/kgt/chooseWork.svg" alt="kgt  design" />
       </div>
       <div class="image">
-        <img src="../assets/kgt/choosePersonell.png" alt="kgt design" />
+        <img src="../assets/kgt/choosePersonell.svg" alt="kgt design" />
       </div>
       <div class="h4 text-left">3. Get selected!</div>
       <div class="h4 text-right">4. Report your time</div>
       <div class="image">
-        <img src="../assets/kgt/reportTime.png" alt="kgt design" />
+        <img src="../assets/kgt/reportTime.svg" alt="kgt design" />
       </div>
     </div>
   </div>
@@ -92,6 +92,29 @@ export default {
     font-style: italic;
   }
 }
+.gallery-flex {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 800px;
+  align-items: center;
+  justify-content: center;
+}
+.gallery-flex > * {
+  flex: 0 0 1;
+  z-index: 3;
+}
+.gallery-flex .image {
+  max-width: 100%;
+  img {
+    max-width: 240px;
+    max-height: 100%;
+
+    @include for-phone-only {
+      max-width: 180px;
+    }
+  }
+}
+
 .gallery-container {
   width: 100%;
   position: relative;
@@ -101,15 +124,17 @@ export default {
 }
 .blue-divider {
   position: absolute;
+  min-width: 100vw;
   img {
     max-width: 100%;
     max-height: 100%;
   }
-}
-.gallery-3-col .image {
-  width: 100%;
-  height: 100%;
-  z-index: 10;
+  @include for-big-desktop-up {
+    display: none;
+  }
+  @include for-phone-only {
+    display: none;
+  }
 }
 
 .text-left {
@@ -119,25 +144,6 @@ export default {
 .text-right {
   text-align: right;
   margin-right: 50px;
-}
-
-.gallery-3-col {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-auto-rows: 460px;
-  max-width: 800px;
-
-  @include for-tablet-landscape-down {
-    grid-auto-rows: 380px;
-  }
-
-  @include for-phone-only {
-    grid-auto-rows: 200px;
-  }
-  img {
-    max-width: 100%;
-    max-height: 100%;
-  }
 }
 
 .gallery-2-col {
