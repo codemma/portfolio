3ea4fcb44bb02dac75cac4193dcb24505bfbd289
codemma <emma.tysk@gmail.com>
2021-01-12 17:41:29 +0100

    deploy

---
+++ precache-manifest.f934577b9630bae26eb91817e365ae63.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62901d051ea97a3b8c6",
+    "url": "/portfolio/css/about.066a597a.css"
+  },
+  {
+    "revision": "ed1e1f4d4ed5cbd7f3e8",
+    "url": "/portfolio/css/app.6a124f3a.css"
+  },
+  {
+    "revision": "bc77b9b57c01e069cd0b6d5ac38c632a",
+    "url": "/portfolio/img/brezza.bc77b9b5.gif"
+  },
+  {
+    "revision": "d206658bb3ac3a0a3ab2cc2983057698",
+    "url": "/portfolio/img/choosePersonell.d206658b.svg"
+  },
+  {
+    "revision": "86df698ebd04c90943034b9a8debf806",
+    "url": "/portfolio/img/chooseWork.86df698e.svg"
+  },
+  {
+    "revision": "f316ac7bf8a1564bf9ae2b5e24fc9463",
+    "url": "/portfolio/img/divider.f316ac7b.svg"
+  },
+  {
+    "revision": "9dc9fba641d3de0f0ecd7b3dcf2e7c18",
+    "url": "/portfolio/img/getApp.9dc9fba6.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio/img/github.76d344f0.svg"
+  },
+  {
+    "revision": "29f526639182772a4a780b26130879a2",
+    "url": "/portfolio/img/preview.29f52663.png"
+  },
+  {
+    "revision": "73a7b14c75b0e4d43a9786bf88416201",
+    "url": "/portfolio/img/preview.73a7b14c.png"
+  },
+  {
+    "revision": "79ec662cafb600f49d05381add6ad0bb",
+    "url": "/portfolio/img/preview.79ec662c.png"
+  },
+  {
+    "revision": "98874fb47e785ef129d93812276ed2e6",
+    "url": "/portfolio/img/preview.98874fb4.png"
+  },
+  {
+    "revision": "bc3120bbda9ede05a677c63a399f208a",
+    "url": "/portfolio/img/reportTime.bc3120bb.svg"
+  },
+  {
+    "revision": "157c75fe873353713dd3198111b0d564",
+    "url": "/portfolio/img/screen-1.157c75fe.png"
+  },
+  {
+    "revision": "91e57386c6f0c301983e10008a709df0",
+    "url": "/portfolio/img/screen-1.91e57386.png"
+  },
+  {
+    "revision": "daff991d6962bd9ab7cc51ed454b07e3",
+    "url": "/portfolio/img/screen-1.daff991d.png"
+  },
+  {
+    "revision": "eb2169d5d7f25bed4df9669b4b45343d",
+    "url": "/portfolio/img/screen-1.eb2169d5.png"
+  },
+  {
+    "revision": "36e998243dd63ad9d44f93cfc2ea5051",
+    "url": "/portfolio/img/screen-2.36e99824.png"
+  },
+  {
+    "revision": "a7fa2ecc0e91b5a0947f5ae7ddc00596",
+    "url": "/portfolio/img/screen-2.a7fa2ecc.png"
+  },
+  {
+    "revision": "d599e4a22093e2324fbd3bdc8be0d257",
+    "url": "/portfolio/img/screen-2.d599e4a2.png"
+  },
+  {
+    "revision": "fd7c2f668d3042786322e670b9e8ebac",
+    "url": "/portfolio/img/screen-2.fd7c2f66.png"
+  },
+  {
+    "revision": "516af9494a89b8a9df6d0616906bb66f",
+    "url": "/portfolio/img/screen-3.516af949.png"
+  },
+  {
+    "revision": "79fb6ca0fe4fd2780911eb865c2cf33b",
+    "url": "/portfolio/img/screen-3.79fb6ca0.png"
+  },
+  {
+    "revision": "abbf6500d16f02edf6c6a77df7eebd07",
+    "url": "/portfolio/img/screen-3.abbf6500.png"
+  },
+  {
+    "revision": "c8f606effc2b72621bad146a7425f9e1",
+    "url": "/portfolio/img/screen-3.c8f606ef.png"
+  },
+  {
+    "revision": "155b2b2ef29d0bdb29524fbcab8a4802",
+    "url": "/portfolio/img/screen-4.155b2b2e.png"
+  },
+  {
+    "revision": "38438d3877d87376e6db35b19bbfe1cd",
+    "url": "/portfolio/img/screen-4.38438d38.png"
+  },
+  {
+    "revision": "934a9efa0dc495415c881304cbaec208",
+    "url": "/portfolio/img/screen-4.934a9efa.png"
+  },
+  {
+    "revision": "d0248fd0c63cbb1f24e827f747936bcd",
+    "url": "/portfolio/img/screen-5.d0248fd0.png"
+  },
+  {
+    "revision": "eab6bc81137c50cf62d647eb2fe5baa4",
+    "url": "/portfolio/img/screen-5.eab6bc81.png"
+  },
+  {
+    "revision": "983d0b9aa27aacdc7ece1c9b12b486f1",
+    "url": "/portfolio/img/screen-6.983d0b9a.png"
+  },
+  {
+    "revision": "9a9ab90424d44a0fbde6c8b0e89af326",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b62901d051ea97a3b8c6",
+    "url": "/portfolio/js/about.b1559d2b.js"
+  },
+  {
+    "revision": "ed1e1f4d4ed5cbd7f3e8",
+    "url": "/portfolio/js/app.c5c3af63.js"
+  },
+  {
+    "revision": "d3c35cbadae3ba3c3de5",
+    "url": "/portfolio/js/chunk-vendors.5019f69b.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  },
+  {
+    "revision": "a729f9152da55fc75c2f2413c34fac2d",
+    "url": "/portfolio/togather.pdf"
+  }
+]);
